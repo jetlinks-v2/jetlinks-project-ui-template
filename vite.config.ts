651ad028532aa7898ee03fc516e10aa7a6456ad3
XieYongHong <18010623010@163.com>
2023-07-21 10:53:37 +0800

    fix: 优化token失效导致登录页白屏

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,21 @@ import { createViteConfig } from '@jetlinks/vite'
 import { defineConfig, UserConfig } from 'vite'
 
 export default defineConfig(async ({ command, mode }): Promise<UserConfig> => {
-  return await createViteConfig(command, mode, process.cwd())
+  return await createViteConfig(
+    command,
+    mode,
+    process.cwd(),
+    {
+      css: {
+        preprocessorOptions: {
+          less: {
+            modifyVars: {
+              'primary-color': '#1DA57A',
+            },
+            javascriptEnabled: true,
+          }
+        }
+      }
+    }
+  )
 })
