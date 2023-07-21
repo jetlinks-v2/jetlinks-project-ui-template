edebce1621447abb56615d1dcd9cf7299e3b8479
100011797 <2642441182@qq.com>
2023-07-21 11:24:41 +0800

    refactor: 角色管理和组织管理

--- src/api/department.ts
+++ src/api/department.ts
@@ -9,28 +9,6 @@ export const updateDepartment_api = (data: any) => request.patch(`/organization`
 // 删除部门
 export const delDepartment_api = (id: string) => request.remove(`/organization/${id}`);
 
-
-// 获取所属产品列表
-export const getDeviceProduct_api = (data: any) => request.get(`/device/product/_query/no-paging`, data);
-// 获取产品列表
-export const getDeviceOrProductList_api = (data: any) => request.post(`/device-product/_query`, data);
-// 获取设备列表
-export const getDeviceList_api = (data: any) => request.post(`/device/instance/_query`, data);
-// 根据产品的id获取产品的权限
-export const getPermission_api = (type: 'device' | 'product', ids: any, id: string) => request.post(`/assets/bindings/${type}/org/${id}/_query`, ids);
-// 获取绑定的权限
-export const getBindingsPermission = (type: 'device' | 'product', ids: string[]) => request.post(`/assets/bindings/${type}`, ids);
-// 获取产品的权限字典
-export const getPermissionDict_api = () => request.get(`/assets/bindings/product/permissions`);
-
-// 部门绑定产品
-export const bindDeviceOrProductList_api = (type: 'device' | 'product', data: any) => request.post(`/assets/bind/${type}`, data);
-// 批量解绑
-export const unBindDeviceOrProduct_api = (type: 'device' | 'product', data: any) => request.post(`/assets/unbind/${type}`, data);
-// 批量更新权限
-export const updatePermission_api = (type: 'device' | 'product', parentId: string, data: any) => request.put(`/assets/permission/${type}/org/${parentId}/_batch`, data);
-
-
 // 用户相关
 // 获取绑定用户列表
 export const getBindUserList_api = (data: any) => request.post(`/user/_query`, data);
--- src/views/system/Department/components/LeftTree.vue
+++ src/views/system/Department/components/LeftTree.vue
@@ -1,328 +1,275 @@
 <template>
-    <div class="left-tree-container">
-        <j-input
-            v-model:value="searchValue"
-            @change="search"
-            placeholder="请输入组织名称"
-            class="search-input"
-            allowClear
+  <FullPage>
+    <j-input
+      v-model:value="searchValue"
+      @change="onSearch"
+      placeholder="请输入组织名称"
+      allowClear
+    >
+      <template #suffix>
+        <AIcon type="SearchOutlined" />
+      </template>
+    </j-input>
+    <PermissionButton
+      type="primary"
+      :hasPermission="`${permission}:add`"
+      @click="openDialog()"
+      style="width: 100%; margin: 12px 0"
+    >
+      新增
+    </PermissionButton>
+    <div class="tree">
+      <j-spin :spinning="loading">
+        <j-tree
+          v-if="treeData.length > 0"
+          :tree-data="treeData"
+          v-model:selected-keys="selectedKeys"
+          v-model:expandedKeys="expandedKeys"
+          :fieldNames="{ key: 'id' }"
+          height="100%"
         >
-            <template #suffix>
-                <AIcon type="SearchOutlined" />
-            </template>
-        </j-input>
-        <div class="add-btn">
-            <PermissionButton
-                type="primary"
-                :hasPermission="`${permission}:add`"
-                @click="openDialog()"
-            >
-                新增
-            </PermissionButton>
-        </div>
-
-        <div class="tree">
-          <j-spin :spinning='loading'>
-              <j-tree
-                  v-if="treeData.length > 0"
-                  :tree-data="treeData"
-                  v-model:selected-keys="selectedKeys"
-                  v-model:expandedKeys="expandedKeys"
-                  :fieldNames="{ key: 'id' }"
-              >
-                  <template #title="{ name, data }">
-                    <div class='department-tree-item-content'>
-                      <span class='title'>
-                        <j-ellipsis>
-                          {{ name }}
-                        </j-ellipsis>
-                      </span>
-                      <span class="func-btns" @click="(e) => e.stopPropagation()">
-                          <PermissionButton
-                              :hasPermission="`${permission}:update`"
-                              type="link"
-                              :tooltip="{
-                                  title: '编辑',
-                              }"
-                              @click="openDialog(data)"
-                          >
-                              <AIcon type="EditOutlined" />
-                          </PermissionButton>
-                          <PermissionButton
-                              :hasPermission="`${permission}:add`"
-                              type="link"
-                              :tooltip="{
-                                  title: '新增子组织',
-                              }"
-                              @click="
-                                  openDialog({
-                                      ...data,
-                                      id: '',
-                                      parentId: data.id,
-                                  })
-                              "
-                          >
-                              <AIcon type="PlusCircleOutlined" />
-                          </PermissionButton>
-                          <PermissionButton
-                              type="link"
-                              :hasPermission="`${permission}:delete`"
-                              :tooltip="{ title: '删除' }"
-                              :popConfirm="{
-                                  title: `确定要删除吗`,
-                                  onConfirm: () => delDepartment(data.id),
-                              }"
-                          >
-                              <AIcon type="DeleteOutlined" />
-                          </PermissionButton>
-                      </span>
-                    </div>
-                  </template>
-              </j-tree>
-              <j-empty v-else description="暂无数据" />
-          </j-spin>
-        </div>
-        <!-- 编辑弹窗 -->
-        <EditDepartmentDialog
-            v-if="dialog.visible"
-            v-model:visible="dialog.visible"
-            :tree-data="sourceTree"
-            :data="dialog.selectItem"
-            @refresh="refresh"
-        />
+          <template #title="{ name, data }">
+            <div class="department-tree-item-content">
+              <div class="title">
+                <j-ellipsis>
+                  {{ name }}
+                </j-ellipsis>
+              </div>
+              <div class="func-btn" @click="(e) => e.stopPropagation()">
+                <PermissionButton
+                  :hasPermission="`${permission}:update`"
+                  type="link"
+                  :tooltip="{
+                    title: '编辑',
+                  }"
+                  style="padding: 0"
+                  @click="openDialog(data)"
+                >
+                  <AIcon type="EditOutlined" />
+                </PermissionButton>
+                <PermissionButton
+                  :hasPermission="`${permission}:add`"
+                  type="link"
+                  :tooltip="{
+                    title: '新增子组织',
+                  }"
+                  @click="
+                    openDialog({
+                      ...data,
+                      id: '',
+                      parentId: data.id,
+                    })
+                  "
+                >
+                  <AIcon type="PlusCircleOutlined" />
+                </PermissionButton>
+                <PermissionButton
+                  type="link"
+                  :hasPermission="`${permission}:delete`"
+                  :tooltip="{ title: '删除' }"
+                  style="padding: 0"
+                  danger
+                  :popConfirm="{
+                    title: `确定要删除吗`,
+                    onConfirm: () => delDepartment(data.id),
+                  }"
+                >
+                  <AIcon type="DeleteOutlined" />
+                </PermissionButton>
+              </div>
+            </div>
+          </template>
+        </j-tree>
+        <j-empty v-else />
+      </j-spin>
     </div>
+    <!-- 编辑弹窗 -->
+    <Save
+      v-if="visible"
+      :tree-data="sourceTree"
+      :data="current"
+      @save="onSave"
+      @close="visible = false"
+    />
+  </FullPage>
 </template>
 
 <script setup lang="ts">
-import { getTreeData_api, delDepartment_api } from '@/api/department';
-import { debounce, cloneDeep, omit } from 'lodash-es';
-import EditDepartmentDialog from './EditDepartmentDialog.vue';
+import { getTreeData_api, delDepartment_api } from '@/api/department'
+import { onlyMessage } from '@jetlinks/utils'
+import { debounce, cloneDeep, omit } from 'lodash-es'
+import Save from './Save.vue'
 import { useRoute } from 'vue-router'
-import { message } from 'jetlinks-ui-components';
+import { ArrayToTree } from '../util'
 
-const permission = 'system/Department';
+const permission = 'system/Department'
 
-const save = useRoute().query.save;
+const save = useRoute().query.save
 
-const emits = defineEmits(['change']);
-const searchValue = ref(''); // 搜索内容
-const loading = ref<boolean>(false); // 数据加载状态
-const sourceTree = ref<any[]>([]); // 源数据
-const treeMap = new Map(); // 数据的map版本
-const treeData = ref<any[]>([]); // 展示的数据
-const selectedKeys = ref<string[]>([]); // 当前选中的项
-const expandedKeys = ref<string[] | number[]>([]);
-
-function getTree(cb?: Function) {
-    loading.value = true;
-    const params = {
-        paging: false,
-        sorts: [{ name: 'sortIndex', order: 'asc' }],
-    } as any;
-    if (searchValue.value) {
-        params.terms = [
-            { column: 'name$LIKE', value: `%${searchValue.value}%` },
-        ];
-    }
-
-    getTreeData_api(params)
-        .then((resp: any) => {
-            selectedKeys.value = [resp.result[0].id];
-            sourceTree.value = resp.result.sort((a: any, b: any) =>
-                a.sortIndex === b.sortIndex
-                    ? b.createTime - a.createTime
-                    : a.sortIndex - b.sortIndex,
-            ); // 报存源数据
-            handleTreeMap(resp.result); // 将树形结构转换为map结构
-            treeData.value = resp.result; // 第一次不用进行过滤
-            cb && cb();
-        })
-        .finally(() => {
-            loading.value = false;
-        });
-}
-
-const ArrayToTree = (list: any[]): any[] => {
-  const treeList: any[] = [];
-  // 所有项都使用对象存储起来
-  const map = {};
-
-  // 建立一个映射关系：通过id快速找到对应的元素
-  list.forEach((item) => {
-    if (!item.children) {
-      item.children = [];
-    }
-    map[item.id] = item;
-  });
-
-  list.forEach((item) => {
-    // 对于每一个元素来说，先找它的上级
-    //    如果能找到，说明它有上级，则要把它添加到上级的children中去
-    //    如果找不到，说明它没有上级，直接添加到 treeList
-    const parent = map[item.parentId];
-    // 如果存在则表示item不是最顶层的数据
-    if (parent) {
-      parent.children.push(item);
-    } else {
-      // 如果不存在 则是顶层数据
-      treeList.push(item);
-    }
-  });
-  // 返回出去
-  return treeList;
-}
-
-const search = debounce(() => {
-    const key = searchValue.value;
-    const treeArray = new Map();
-    if (key) {
-        const searchTree: string[] = [];
-        treeMap.forEach((item) => {
-            if (item.name.includes(key)) {
-                searchTree.push(item.parentId);
-                treeArray.set(item.id, item);
-            }
-        });
-        dig(searchTree);
-        treeData.value = ArrayToTree(cloneDeep([...treeArray.values()]));
-    } else {
-        treeData.value = ArrayToTree(cloneDeep([...treeMap.values()]));
-        expandedKeys.value = [];
-    }
-
-    function dig(_data: any[]): any {
-        const pIds: string[] = [];
-        if (!_data.length) return;
-        _data.forEach((item) => {
-            if (treeMap.has(item)) {
-                const _item = treeMap.get(item);
-                pIds.push(_item.parentId);
-                treeArray.set(item, _item);
-            }
-        });
-    }
-}, 500);
-// 将树形数组转化为map形式，以便筛选时操作
-function handleTreeMap(_data: any[]) {
-    if (_data) {
-        _data.map((item) => {
-            treeMap.set(item.id, omit(cloneDeep(item), ['children']));
-            if (item.children) {
-                handleTreeMap(item.children);
-            }
-        });
-    }
-}
-// 删除部门
-function delDepartment(id: string) {
-    delDepartment_api(id).then(() => {
-        message.success('操作成功');
-        getTree();
-    });
-}
-function refresh(id: string) {
-    // @ts-ignore
-    window?.onTabSaveSuccess && window.onTabSaveSuccess(id);
-    setTimeout(() => window.close(), 300);
-    getTree();
-}
+const emits = defineEmits(['change'])
+const searchValue = ref('') // 搜索内容
+const loading = ref<boolean>(false) // 数据加载状态
+const sourceTree = ref<any[]>([]) // 源数据
+const treeMap = new Map() // 数据的map版本
+const treeData = ref<any[]>([]) // 展示的数据
+const selectedKeys = ref<string[]>([]) // 当前选中的项
+const expandedKeys = ref<string[] | number[]>([])
 
 // 弹窗
-const dialog = reactive({
-    visible: false,
-    selectItem: {},
-});
+const visible = ref<boolean>(false)
+const current = ref<any>({})
+
+// 获取数据
+const getTree = (cb?: Function) => {
+  loading.value = true
+  const params = {
+    paging: false,
+    sorts: [{ name: 'sortIndex', order: 'asc' }],
+  } as any
+  if (searchValue.value) {
+    params.terms = [{ column: 'name$LIKE', value: `%${searchValue.value}%` }]
+  }
+
+  getTreeData_api(params)
+    .then((resp: any) => {
+      selectedKeys.value = [resp.result[0].id]
+      sourceTree.value = resp.result.sort((a: any, b: any) =>
+        a.sortIndex === b.sortIndex
+          ? b.createTime - a.createTime
+          : a.sortIndex - b.sortIndex,
+      ) // 报存源数据
+      handleTreeMap(resp.result) // 将树形结构转换为map结构
+      treeData.value = resp.result // 第一次不用进行过滤
+      cb && cb()
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
+// 搜索
+const onSearch = debounce(() => {
+  const key = searchValue.value
+  const treeArray = new Map()
+  if (key) {
+    const searchTree: string[] = []
+    treeMap.forEach((item) => {
+      if (item.name.includes(key)) {
+        searchTree.push(item.parentId)
+        treeArray.set(item.id, item)
+      }
+    })
+    dig(searchTree)
+    treeData.value = ArrayToTree(cloneDeep([...treeArray.values()]))
+  } else {
+    treeData.value = ArrayToTree(cloneDeep([...treeMap.values()]))
+    expandedKeys.value = []
+  }
+
+  function dig(_data: any[]): any {
+    const pIds: string[] = []
+    if (!_data.length) return
+    _data.forEach((item) => {
+      if (treeMap.has(item)) {
+        const _item = treeMap.get(item)
+        pIds.push(_item.parentId)
+        treeArray.set(item, _item)
+      }
+    })
+  }
+}, 500)
+
+// 将树形数组转化为map形式，以便筛选时操作
+function handleTreeMap(_data: any[]) {
+  if (_data) {
+    _data.map((item) => {
+      treeMap.set(item.id, omit(cloneDeep(item), ['children']))
+      if (item.children) {
+        handleTreeMap(item.children)
+      }
+    })
+  }
+}
+// 删除部门
+const delDepartment = (id: string) => {
+  delDepartment_api(id).then((resp) => {
+    if (resp.success) {
+      onlyMessage('操作成功')
+      getTree()
+    }
+  })
+}
+
+// 刷新
+const onSave = (id: string) => {
+  visible.value = false
+  current.value = {}
+  // @ts-ignore
+  window?.onTabSaveSuccess && window.onTabSaveSuccess(id)
+  setTimeout(() => window.close(), 300)
+  getTree()
+}
 
 const openDialog = (row: any = {}) => {
-    // 计算默认排序值，为子列表中最大的排序值+1
-    let sortIndex = row.sortIndex || 1;
-    if (!row.id) {
-        let childrens = [] as any[];
-        if (row.parentId) {
-            childrens = row.children;
-        } else childrens = treeData.value;
-        const indexs =
-            childrens?.length > 0
-                ? childrens?.map((item) => item.sortIndex)
-                : [0];
-        sortIndex = Math.max(...indexs) + 1;
-    }
+  // 计算默认排序值，为子列表中最大的排序值+1
+  let sortIndex = row.sortIndex || 1
+  if (!row.id) {
+    let children = [] as any[]
+    if (row.parentId) {
+      children = row.children
+    } else children = treeData.value
+    const _index =
+      children?.length > 0 ? children?.map((item) => item.sortIndex) : [0]
+    sortIndex = Math.max(..._index) + 1
+  }
 
-    dialog.selectItem = { ...row, sortIndex };
-    dialog.visible = true;
-};
+  current.value = { ...row, sortIndex }
+  visible.value = true
+}
 
-watch(() => selectedKeys.value, (n) => {
-    emits('change', n?.[0]);
-}, {
-    immediate: true
-});
+watch(
+  () => selectedKeys.value,
+  (n) => {
+    emits('change', n?.[0])
+  },
+  {
+    immediate: true,
+  },
+)
 
 onMounted(() => {
-    getTree(save ? openDialog : undefined);
+  getTree(save ? openDialog : undefined)
 })
 </script>
 
 <style lang="less" scoped>
-.left-tree-container {
-    padding-right: 24px;
-    border-right: 1px solid #f0f0f0;
-    display: flex;
-    height: 100%;
-    flex-direction: column;
+.department-tree-item-content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 25px;
 
-    .add-btn {
-        margin: 24px 0;
+  .title {
+    width: calc(100% - 80px);
+  }
+  .func-btn {
+    display: none;
+    font-size: 14px;
+    width: 80px;
+  }
+}
 
-        :deep(.ant-btn-primary) {
-            width: 100%;
-        }
-    }
+:deep(.ant-tree-treenode) {
+  width: 100%;
+  .ant-tree-node-content-wrapper {
+    flex: 1 1 auto;
 
-    :deep(.ant-tree-treenode) {
-        width: 100%;
-        .ant-tree-node-content-wrapper {
-            flex: 1 1 auto;
-
-            .ant-tree-title {
-                &:hover {
-                    .func-btns {
-                        display: block;
-                    }
-                }
-            }
-        }
-    }
-
-
-    .tree {
-      overflow-y: auto;
-      overflow-x: hidden;
-
-      .department-tree-item-content {
-        display: flex;
-        align-items: center;
-
-        .title {
-          width: calc(100% - 80px);
-        }
-        .func-btns {
-          display: none;
-          font-size: 14px;
-          width: 80px;
-          :deep(.ant-btn-link) {
-            padding: 0 4px;
-            height: 24px;
-          }
+    .ant-tree-title {
+      &:hover {
+        .func-btn {
+          display: block;
         }
       }
-
-      .loading {
-        display: flex;
-        width: 100%;
-        justify-content: center;
-        margin-top: 20px;
-      }
     }
+  }
 }
 </style>
