29ea8025a618ced38fd2d6ba98f23123719de348
leiqiaochu <leiqiaochu@aossci.com>
2024-02-18 10:23:56 +0800

    fix: 修改角色权限方法

--- src/views/system/Role/Detail/index.vue
+++ src/views/system/Role/Detail/index.vue
@@ -2,7 +2,7 @@
     <page-container>
         <div class="details-container">
             <j-tabs v-model:activeKey="activeKey">
-                <j-tab-pane key="1" tab="权限分配"><Permiss /></j-tab-pane>
+                <j-tab-pane key="1" tab="权限分配"><Permission /></j-tab-pane>
                 <j-tab-pane key="2" tab="用户管理"><User /></j-tab-pane>
             </j-tabs>
         </div>
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts" name="RoleDetail">
-import Permiss from './Permiss/index.vue';
+import Permission from './Permission/index.vue';
 import User from './User/index.vue';
 
 const activeKey = ref('1');
