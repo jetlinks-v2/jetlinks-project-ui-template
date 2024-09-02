b1f3c603bc042190b6abc76941f4652b73bb12c0
XieYongHong <18010623010@163.com>
2024-09-02 10:33:46 +0800

    remove: 删除data-collect模块；删除未使用的api接口

--- src/api/notice/template.ts
+++ src/api/notice/template.ts
@@ -10,19 +10,9 @@ export default {
     // 修改
     update: (data: any) => request.patch(`/notifier/template`, data),
     del: (id: any) => request.remove(`/notifier/template/${id}`),
-    getConfig: (data: any) => request.post(`/notifier/config/_query/no-paging?paging=false`, data),
     getTemplateDetail: (id: string) => request.get<any>(`/notifier/template/${id}/detail`),
     debug: (data: any, configId: string, templateId: string) => request.post(`/notifier/${configId}/${templateId}/_send`, data),
-    getHistory: (data: any, id: string) => request.post(`/notify/history/template/${id}/_query`, data),
-    // 钉钉/微信, 根据配置获取部门和用户
-    getDept: (type: string, id: string) => request.get<any>(`/notifier/${type}/corp/${id}/departments`),
-    getUser: (type: string, id: string) => request.get<any>(`/notifier/${type}/corp/${id}/users`),
     // 微信获取标签推送
     getTags: (id: string) => request.get<any>(`/notifier/wechat/corp/${id}/tags`),
-    // 语音/短信获取阿里云模板
-    getAliTemplate: (id: any) => request.get(`/notifier/sms/aliyun/${id}/templates`),
-    // 短信获取签名
-    getSigns: (id: any) => request.get(`/notifier/sms/aliyun/${id}/signs`),
-    getListByConfigId: (id: string, data: any): any => request.post(`/notifier/template/${id}/_query`, data),
     getListVariableByConfigId: (id: string, data?: any): any => request.post(`/notifier/template/${id}/detail/_query`, data),
 }
--- src/views/DataCollect/Dashboard/typings.d.ts
+++
@@ -1,8 +0,0 @@
-export type Footer = {
-    title: string;
-    value: number | string;
-    total: number | string;
-    status?: 'default' | 'error' | 'success' | 'warning' | 'processing' | '';
-    type: string;
-    label: string;
-};
