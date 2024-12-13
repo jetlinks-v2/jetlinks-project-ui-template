a410d387e6cd504f5f0361b3b1f6525d916ffce2
XieYongHong <18010623010@163.com>
2024-12-13 18:47:33 +0800

    feat: 新增模块中的国际化

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -3,6 +3,18 @@ import { createI18n } from 'vue-i18n'
 import zhCn from './lang/zh.json'
 import enUS from './lang/en.json'
 
+const zhFiles = import.meta.glob('../modules/*/locales/lang/zh.json', {eager: true})
+const enFiles = import.meta.glob('../modules/*/locales/lang/en.json', {eager: true})
+
+Object.keys(zhFiles).forEach((key) => {
+  const result = (zhFiles[key] as any)?.default
+  Object.assign(zhCn, result)
+})
+
+Object.keys(enFiles).forEach((key) => {
+  const result = (enFiles[key] as any)?.default
+  Object.assign(enUS, result)
+})
 
 const messages = {
   zh: zhCn,
@@ -12,11 +24,11 @@ const messages = {
 const language = (navigator.language || 'en').toLocaleLowerCase() // 这是获取浏览器的语言
 
 const i18n = createI18n({
+  legacy: false,
   silentTranslationWarn: true,
   locale: localStorage.getItem('lang') || language.split('-')[0] || 'en',
   fallbackLocale: language,
   messages,
-  legacy: false
 })
 
 export default i18n
