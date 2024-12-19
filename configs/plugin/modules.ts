042418b356e324133746a3f6cd7813f84a0d8f79
holt1028 <lunghi.lin@qq.com>
2024-12-19 14:06:00 +0800

    fix: 修复无子模块下扫描配置文件报错

--- configs/plugin/modules.ts
+++ configs/plugin/modules.ts
@@ -7,15 +7,18 @@ const modulesBasePath = 'src/modules'
 function registerModulesAlias() {
   const modulesAlias = {}
   const pattern = path.resolve(rootPath, modulesBasePath)
-  const folders = fs.readdirSync(pattern)
-  folders?.map((name) => {
-    const result = fs.readFileSync(path.resolve(rootPath, modulesBasePath, `${name}/config.json`), 'utf-8')
-    const content = JSON.parse(result)
-    if (content.aliasName) {
-      modulesAlias[content.aliasName] = path.resolve(modulesBasePath, name)
-    }
-  })
+  try {
+    const folders = fs.readdirSync(pattern)
+    folders?.map((name) => {
+      const result = fs.readFileSync(path.resolve(rootPath, modulesBasePath, `${name}/config.json`), 'utf-8')
+      const content = JSON.parse(result)
+      if (content.aliasName) {
+        modulesAlias[content.aliasName] = path.resolve(modulesBasePath, name)
+      }
+    })
+  } catch (error) {
+    console.warn(`Failed to load ${modulesBasePath} configuration file!`)
+  }
   return modulesAlias
 }
-
 export { registerModulesAlias }
