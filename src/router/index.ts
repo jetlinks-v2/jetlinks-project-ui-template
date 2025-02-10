c9b2bc21906af3f8f43200f7ffba61ba87ec7fec
XieYongHong <18010623010@163.com>
2025-02-10 14:42:27 +0800

    feat: 添加demo页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,19 @@ import {
   createWebHashHistory,
 } from 'vue-router'
 import { getToken, removeToken } from '@jetlinks-web/utils'
-import { NOT_FIND_ROUTE, LOGIN_ROUTE } from './basic'
+import { NOT_FIND_ROUTE, LOGIN_ROUTE, DEMO } from './basic'
 import {useUserStore} from "@/store/user";
 import {useSystemStore} from "@/store/system";
 import {useMenuStore} from "@/store/menu";
 
-let TokenFilterRoute: string[] = []
+let TokenFilterRoute: string[] = [ DEMO.path ]
 
-let FilterPath: string[] = []
+let FilterPath: string[] = [DEMO.path]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
-    LOGIN_ROUTE
+    LOGIN_ROUTE, DEMO
   ],
   scrollBehavior(to, form, savedPosition) {
     return savedPosition || {top: 0}
