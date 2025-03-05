24519513a4a3b9191afe020103628876d11b16e3
holt230 <lunghi.lin@qq.com>
2025-03-05 15:22:46 +0800

    feat: 新增子模块less通用变量解析

--- configs/plugin/modules.ts
+++ configs/plugin/modules.ts
@@ -11,10 +11,12 @@ function registerModulesAlias() {
     const folders = fs.readdirSync(pattern)
     folders?.map((name) => {
       try {
-        const result = fs.readFileSync(path.resolve(rootPath, modulesBasePath, `${name}/config.json`), 'utf-8')
-        const content = JSON.parse(result)
-        if (content.aliasName) {
-          modulesAlias[content.aliasName] = path.resolve(modulesBasePath, name)
+        if (fs.existsSync(path.resolve(rootPath, modulesBasePath, `${name}/config.json`))) {
+          const result = fs.readFileSync(path.resolve(rootPath, modulesBasePath, `${name}/config.json`), 'utf-8')
+          const content = JSON.parse(result)
+          if (content.aliasName) {
+            modulesAlias[content.aliasName] = path.resolve(modulesBasePath, name)
+          }
         }
       } catch (error) {
         console.warn(`Failed to load ${modulesBasePath} configuration file!`)
@@ -24,7 +26,20 @@ function registerModulesAlias() {
     console.warn(`Failed to load ${modulesBasePath} Folder`)
   }
 
-  console.log(modulesAlias)
   return modulesAlias
 }
-export { registerModulesAlias }
+
+function registerModulesLessVariable() {
+  const pattern = path.resolve(rootPath, modulesBasePath)
+  try {
+    const folders = fs.readdirSync(pattern)
+    return folders
+      ?.filter((module) => fs.existsSync(path.resolve(`${pattern}/${module}/style/variable.less`)))
+      .map((module) => `@import (reference) "${path.resolve(`${pattern}/${module}/style/variable.less`)}";`)
+      .join('\n')
+  } catch (error) {
+    console.warn(`Failed to load ${modulesBasePath} style/variable.less`)
+  }
+}
+
+export { registerModulesAlias, registerModulesLessVariable }
--- vite.config.ts
+++ vite.config.ts
@@ -6,143 +6,139 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import { VueAmapResolver } from '@vuemap/unplugin-resolver'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
-import {
-    createStyleImportPlugin,
-    AndDesignVueResolve,
-} from 'vite-plugin-style-import'
+import { createStyleImportPlugin, AndDesignVueResolve } from 'vite-plugin-style-import'
 import * as path from 'path'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor'
-import { optimizeDeps, registerModulesAlias, backupModulesFile, restoreModulesFile, updateModulesFile, handleRestoreModulesFile } from './configs/plugin'
+import {
+  optimizeDeps,
+  restoreModulesFile,
+  backupModulesFile,
+  updateModulesFile,
+  handleRestoreModulesFile,
+  registerModulesAlias,
+  registerModulesLessVariable
+} from './configs/plugin'
 import progress from 'vite-plugin-progress'
 
-process.on('SIGINT', handleRestoreModulesFile);
-process.on('SIGTERM', handleRestoreModulesFile);
-process.on('uncaughtException', handleRestoreModulesFile);
-process.on('unhandledRejection', handleRestoreModulesFile);
+process.on('SIGINT', handleRestoreModulesFile)
+process.on('SIGTERM', handleRestoreModulesFile)
+process.on('uncaughtException', handleRestoreModulesFile)
+process.on('unhandledRejection', handleRestoreModulesFile)
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
-    const env: Partial<ImportMetaEnv> = loadEnv(mode, process.cwd())
-    let modulesName
-    let isModule = false
+  const env: Partial<ImportMetaEnv> = loadEnv(mode, process.cwd())
+  let modulesName
+  let isModule = false
 
-    if (String(command) === 'build') {
-        backupModulesFile();
-        modulesName = process.argv.find(arg => arg.startsWith('--modules='))?.split('=')[1] || '*';
-        updateModulesFile(modulesName)
-    }
+  if (String(command) === 'build') {
+    backupModulesFile()
+    modulesName = process.argv.find((arg) => arg.startsWith('--modules='))?.split('=')[1] || '*'
+    updateModulesFile(modulesName)
+  }
 
-    isModule = modulesName && !['**', 'no-modules'].includes(modulesName)
-    const tagName = `micro-app-${isModule ? modulesName : 'default'}`
+  isModule = modulesName && !['**', 'no-modules'].includes(modulesName)
+  const tagName = `micro-app-${isModule ? modulesName : 'default'}`
 
-    return {
-        base: './',
-        define: {
-            'import.meta.env.tagName': JSON.stringify(tagName)
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, 'src'),
-                ...registerModulesAlias()
-            },
-        },
-        build: {
-            outDir: isModule ? `src/modules/${modulesName}/dist` : 'dist',
-            assetsDir: isModule ? `src/modules/${modulesName}/assets` : 'assets',
-            sourcemap: false,
-            cssCodeSplit: false,
-            manifest: true,
-            chunkSizeWarningLimit: 2000,
-            assetsInlineLimit: 4000,
-            rollupOptions: {
-                output: {
-                    entryFileNames: `assets/[name].[hash].js`,
-                    chunkFileNames: `assets/[name].[hash].js`,
-                    assetFileNames: (pre) => {
-                        const fileType = pre.name.split('.')?.pop()
-                        if (['png', 'svg', 'ico', 'jpg'].includes(fileType)) {
-                            return `assets/[name].[ext]`
-                        }
-                        return `assets/[name].[hash].[ext]`
-                    },
-                    compact: true,
-                    manualChunks: {
-                        vue: ['vue', 'vue-router', 'pinia'],
-                    },
-                },
-            },
-        },
-        plugins: [
-            vue(),
-            vueJsx(),
-            VueSetupExtend(),
-            monacoEditorPlugin({}),
-            optimizeDeps(),
-            Components({
-                resolvers: [
-                    AntDesignVueResolver({
-                        resolveIcons: true,
-                        importStyle: 'less',
-                    }),
-                    VueAmapResolver(),
-                ],
-                directoryAsNamespace: true,
-            }),
-            AutoImport({
-                imports: ['vue', 'vue-router'],
-                dts: 'src/auto-imports.d.ts',
-                resolvers: [VueAmapResolver()],
-            }),
-            createStyleImportPlugin({
-                resolves: [AndDesignVueResolve()],
-            }),
-            progress(),
-            {
-                name: 'vite-plugin-error-handler',
-                buildEnd(error) {
-                    if (error) {
-                        console.error('Build failed:', error);
-                        handleRestoreModulesFile();
-                    }
-                }
-            },
-            restoreModulesFile()
-        ],
-        server: {
-            host: '0.0.0.0',
-            port: Number(env.VITE_PORT),
-            proxy: {
-                [env.VITE_APP_BASE_API]: {
-                    target: env.VITE_APP_DEV_PROXY_URL,
-                    ws: true,
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), ''),
-                }
+  return {
+    base: './',
+    define: {
+      'import.meta.env.tagName': JSON.stringify(tagName)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        ...registerModulesAlias()
+      }
+    },
+    build: {
+      outDir: isModule ? `src/modules/${modulesName}/dist` : 'dist',
+      assetsDir: isModule ? `src/modules/${modulesName}/assets` : 'assets',
+      sourcemap: false,
+      cssCodeSplit: false,
+      manifest: true,
+      chunkSizeWarningLimit: 2000,
+      assetsInlineLimit: 4000,
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].[hash].js`,
+          chunkFileNames: `assets/[name].[hash].js`,
+          assetFileNames: (pre) => {
+            const fileType = pre.name.split('.')?.pop()
+            if (['png', 'svg', 'ico', 'jpg'].includes(fileType)) {
+              return `assets/[name].[ext]`
             }
-        },
-        css: {
-            preprocessorOptions: {
-                less: {
-                    modifyVars: {
-                        'root-entry-name': 'variable',
-                        hack: `true; @import (reference) "${ path.resolve(
-                            'src/style/variable.less',
-                        ) }";`,
-                    },
-                    javascriptEnabled: true,
-                },
-            },
-        },
-        optimizeDeps: {
-            include: [
-                'pinia',
-                'vue-router',
-                'axios',
-                'lodash-es',
-                '@vueuse/core',
-                'echarts',
-                'dayjs',
-            ],
-        },
+            return `assets/[name].[hash].[ext]`
+          },
+          compact: true,
+          manualChunks: {
+            vue: ['vue', 'vue-router', 'pinia']
+          }
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      VueSetupExtend(),
+      monacoEditorPlugin({}),
+      optimizeDeps(),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            resolveIcons: true,
+            importStyle: 'less'
+          }),
+          VueAmapResolver()
+        ],
+        directoryAsNamespace: true
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        dts: 'src/auto-imports.d.ts',
+        resolvers: [VueAmapResolver()]
+      }),
+      createStyleImportPlugin({
+        resolves: [AndDesignVueResolve()]
+      }),
+      progress(),
+      {
+        name: 'vite-plugin-error-handler',
+        buildEnd(error) {
+          if (error) {
+            console.error('Build failed:', error)
+            handleRestoreModulesFile()
+          }
+        }
+      },
+      restoreModulesFile()
+    ],
+    server: {
+      host: '0.0.0.0',
+      port: Number(env.VITE_PORT),
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_APP_DEV_PROXY_URL,
+          ws: true,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^${env.VITE_APP_BASE_API}`), '')
+        }
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {
+            'root-entry-name': 'variable',
+            hack: `true; @import (reference) "${path.resolve('src/style/variable.less')}";
+                        ${registerModulesLessVariable()}`
+          },
+          javascriptEnabled: true
+        }
+      }
+    },
+    optimizeDeps: {
+      include: ['pinia', 'vue-router', 'axios', 'lodash-es', '@vueuse/core', 'echarts', 'dayjs']
     }
+  }
 })
