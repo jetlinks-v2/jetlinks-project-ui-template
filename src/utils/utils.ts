a5dae52a67f395fa4cca57115d5ebab6608c82cb
XieYongHong <18010623010@163.com>
2025-07-25 18:08:57 +0800

    fix: 优化网关远程访问方式

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
 // 是否不是community版本
 import {getToken} from "@jetlinks-web/utils";
+import {BASE_API} from "@jetlinks-web/constants";
 import { useSystemStore } from '@/store'
 
 export const isNoCommunity = !(localStorage.getItem('version_code') === 'community');
@@ -7,5 +8,6 @@ export const isNoCommunity = !(localStorage.getItem('version_code') === 'communi
 export const openEdgeUrl = (id: string) => {
   const systemStore = useSystemStore()
 
-  window.open(`/ui/edge/cloud/default/index.html/#/login?token=${getToken()}&thingId=${id}&deviceId=${id}&terminal=cloud-pc&thingType=device`)
+  // window.open(`/ui/edge/cloud/default/index.html/#/login?token=${getToken()}&thingId=${id}&deviceId=${id}&terminal=cloud-pc&thingType=device`)
+  window.open(`${BASE_API}/ui/edge/cloud/default/#/login?token=${getToken()}&thingId=${id}&deviceId=${id}&terminal=cloud-pc&thingType=device&proxy=${BASE_API}`)
 }
